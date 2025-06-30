dce902f1d0d428ce63f60b62cca8ff9cf4c6335f
kevindwi <kevindwi356@gmail.com>
2025-06-30 15:58:07 +0700

    feat:add params and icons

--- client/src/componets/Toast.tsx
+++ client/src/componets/Toast.tsx
@@ -1,54 +1,112 @@
-export const Toast = () => {
+interface ToastType {
+  title: string;
+  text: string;
+  type?: "success" | "error" | "info" | "warning";
+}
+
+const Success = () => {
+  return (
+    <>
+      <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg">
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+        </svg>
+        <span className="sr-only">Check icon</span>
+      </div>
+    </>
+  );
+};
+
+const Error = () => {
+  return (
+    <>
+      <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg">
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+        </svg>
+        <span className="sr-only">Error icon</span>
+      </div>
+    </>
+  );
+};
+
+const Info = () => {
+  return (
+    <>
+      <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+          />
+        </svg>
+        <span className="sr-only">Info icon</span>
+      </div>
+    </>
+  );
+};
+
+const Warning = () => {
+  return (
+    <>
+      <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg">
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+        </svg>
+        <span className="sr-only">Warning icon</span>
+      </div>
+    </>
+  );
+};
+
+export const Toast = ({ data }: { data: ToastType }) => {
   return (
     <div
       id="toast-interactive"
       className="w-full text-gray-500 bg-white"
       role="alert"
     >
-      <div className="flex">
-        <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg ">
-          <svg
-            className="w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 18 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M16 1v5h-5M2 19v-5h5m10-4a8 8 0 0 1-14.947 3.97M1 10a8 8 0 0 1 14.947-3.97"
-            />
-          </svg>
-          <span className="sr-only">Refresh icon</span>
-        </div>
+      <div className="flex items-center">
+        {data.type === "success" ? (
+          <Success />
+        ) : data.type === "error" ? (
+          <Error />
+        ) : data.type === "warning" ? (
+          <Warning />
+        ) : (
+          <Info />
+        )}
         <div className="ms-3 text-sm font-normal">
           <span className="mb-1 text-sm font-semibold text-gray-900">
-            Update available
+            {data.title}
           </span>
-          <div className="mb-2 text-sm font-normal">
-            A new software version is available for download.
-          </div>
-          <div className="grid grid-cols-2 gap-2">
-            <div>
-              <a
-                href="#"
-                className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
-              >
-                Update
-              </a>
-            </div>
-            <div>
-              <a
-                href="#"
-                className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200"
-              >
-                Not now
-              </a>
-            </div>
-          </div>
+          <div className="text-sm font-normal">{data.text}</div>
         </div>
       </div>
     </div>
